e9c69e9b468fc7520977d5dc23dd7c8f94c30291
jose.siles <jose.siles@LAPTOP-SUOGFJU0>
2023-02-08 18:27:06 -0600

    Creacion del proyecto

---
+++ FideBus/src/main/java/com/mycompany/fidebus/FideBus.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fidebus;
+
+/**
+ *
+ * @author jose.siles
+ */
+public class FideBus {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
