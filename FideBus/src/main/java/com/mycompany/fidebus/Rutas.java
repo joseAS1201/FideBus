fbabb3d97bcdd6858caeff90c68386a06f595250
jose.siles <jose.siles@LAPTOP-SUOGFJU0>
2023-03-24 11:30:39 -0600

    Creacion de la clase rutas

---
+++ FideBus/src/main/java/com/mycompany/fidebus/Rutas.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.fidebus;
+
+/**
+ *
+ * @author jose.siles
+ */
+public class Rutas {
+    
+    private int a;
+    private int b;
+
+    public Rutas(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public Rutas() {
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+    
+    
+    
+}
