d8374fa8892913f1038df87fbbd8002c129a60e9
BreytonD6 <breytond1@hotmail.com>
2023-03-24 11:36:56 -0600

    prueba hola

---
+++ Fidebu/src/javaapplication27/JavaApplication27.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication27;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author bdgam
+ */
+public class JavaApplication27 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        JOptionPane.showMessageDialog(null, "Hola");
+        
+        // TODO code application logic here
+    }
+    
+}
